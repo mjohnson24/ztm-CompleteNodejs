553309f5cfe40c1a8bac67eb0f76c65f86281c43
mark johnson <mjohnson24@comcast.net>
2025-01-19 17:02:13 -0600

    set package.json so i can run one command and build client to server/public and run server

--- server/src/app.js
+++ server/src/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 
@@ -8,7 +9,17 @@ const app = express();
 app.use(cors({
 	origin: 'http://localhost:3000',
   }));
-app.use(express.json()); // Parse incoming JSON from the body of incoming requests
+  // Parse incoming JSON from the body of incoming requests
+app.use(express.json()); 
+
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+// Use the planets router
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
